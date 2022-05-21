42e2bfa32f2a322ab65269d838662787938147e5
KWY0218 <wodrk1001@gmail.com>
2022-05-22 03:08:34 +0900

    [FEAT] Ui Login

--- app/src/main/java/com/sopt/soptkathon/ui/login/LoginActivity.kt
+++ app/src/main/java/com/sopt/soptkathon/ui/login/LoginActivity.kt
@@ -1,19 +1,18 @@
 package com.sopt.soptkathon.ui.login
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.sopt.soptkathon.MainApp
 import com.sopt.soptkathon.R
 import com.sopt.soptkathon.databinding.ActivityLoginBinding
-import com.sopt.soptkathon.ui.main.MainActivity
-import com.sopt.soptkathon.util.shortToast
-import kotlinx.coroutines.launch
+import java.util.regex.Pattern
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -30,23 +29,30 @@ class LoginActivity : AppCompatActivity() {
                     lifecycleOwner = this@LoginActivity
                 }
 
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.eventFlow.collect { event ->
-                    handleEvent(event)
-                }
-            }
+        viewModel.uiEventFlow
+            .flowWithLifecycle(lifecycle)
+            .onEach(this::handleEvent)
+            .launchIn(lifecycleScope)
+
+        binding.btnLoginLogin.setOnClickListener {
+            Log.d("asdfasfas","${viewModel.isInputEmpty.value}")
+            if (!Pattern.matches("^[가-힣]*\$", viewModel.name.value)) binding.tilLogin.error =
+                "특수문자를 사용할 수 없습니다." else binding.tilLogin.error = null
+            if (!Pattern.matches(
+                    "^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})\$",
+                    viewModel.phoneNumber.value
+                )
+            ) binding.tilLoginPhone.error = "전화번호를 다시 입력해주세요" else binding.tilLoginPhone.error =
+                null
         }
     }
 
     private fun handleEvent(loginEvent: LoginEvent) {
         when (loginEvent) {
-            is LoginEvent.ShowToast -> {
-                shortToast(loginEvent.msg)
-            }
             is LoginEvent.GoMain -> {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+                Log.d("asdfasfas", "asdfmlaksmf;")
+//                startActivity(Intent(this, MainActivity::class.java))
+//                finish()
             }
         }
     }
