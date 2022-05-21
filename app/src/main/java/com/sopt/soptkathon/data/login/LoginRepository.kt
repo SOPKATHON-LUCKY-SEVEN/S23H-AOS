008180a9998947dbb4e3293ddd24a66f23430dc6
KWY0218 <wodrk1001@gmail.com>
2022-05-21 23:24:48 +0900

    [FEAT] 자동로그인 추가

---
+++ app/src/main/java/com/sopt/soptkathon/data/login/LoginRepository.kt
@@ -0,0 +1,32 @@
+package com.sopt.soptkathon.data.login
+
+import android.content.Context
+import android.util.Log
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class LoginRepository(private val applicationContext: Context) {
+    private val Context.dataStore by preferencesDataStore(name = DATASTORE)
+    private val isAutoLoginKey = intPreferencesKey(IS_AUTO_LOGIN)
+
+    fun isAutoLogin(): Flow<Int> {
+        val isAutoLogin: Flow<Int> = applicationContext.dataStore.data.map {
+            it[isAutoLoginKey] ?: -1
+        }
+        return isAutoLogin
+    }
+
+    suspend fun setAutoLogin(isAutoLogin: Int) {
+        applicationContext.dataStore.edit { preferences ->
+            preferences[isAutoLoginKey] = isAutoLogin
+        }
+    }
+
+    companion object {
+        const val DATASTORE = "DATASTORE"
+        const val IS_AUTO_LOGIN = "IS_AUTO_LOGIN"
+    }
+}
