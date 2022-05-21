0cd1f5b6fa880bbd3ff5917fc066459f0e9677ce
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-05-22 08:11:51 +0900

    Code Fix

--- app/src/main/java/com/sopt/soptkathon/MainApp.kt
+++ app/src/main/java/com/sopt/soptkathon/MainApp.kt
@@ -1,8 +1,14 @@
 package com.sopt.soptkathon
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import com.sopt.soptkathon.data.login.LoginRepository
 
 class MainApp : Application() {
     val repository by lazy { LoginRepository(applicationContext) }
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+    }
 }
