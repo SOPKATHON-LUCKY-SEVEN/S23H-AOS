f49ec55d4b6bd9095a8b362b4edf955019be8402
권용민 <briandr@naver.com>
2022-05-22 00:18:42 +0900

    [FEAT] 메인 - 뷰모델, 어댑터, 아이템뷰 추가

--- app/src/main/java/com/sopt/soptkathon/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/soptkathon/ui/main/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.sopt.soptkathon.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.sopt.soptkathon.R
+import androidx.appcompat.app.AppCompatActivity
 import com.sopt.soptkathon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var adapter : MainAdapter
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MainAdapter
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         initAdapter()
     }
 
-    private fun initAdapter(){
+    private fun initAdapter() {
         //TODO
     }
 }
