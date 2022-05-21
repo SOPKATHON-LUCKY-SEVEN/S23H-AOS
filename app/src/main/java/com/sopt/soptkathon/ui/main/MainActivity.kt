81453f7c7fb8ef649ae3575637ec335f316d0ea9
권용민 <briandr@naver.com>
2022-05-21 23:38:18 +0900

    [FEAT] 메인 - 뷰모델, 어댑터, 아이템뷰 추가

--- app/src/main/java/com/sopt/soptkathon/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/soptkathon/ui/main/MainActivity.kt
@@ -2,11 +2,24 @@ package com.sopt.soptkathon.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.sopt.soptkathon.R
+import com.sopt.soptkathon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var adapter : MainAdapter
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+    }
+
+    private fun initAdapter(){
+        //TODO
     }
 }
---
+++ app/src/main/java/com/sopt/soptkathon/ui/main/MainAdapter.kt
@@ -0,0 +1,46 @@
+package com.sopt.soptkathon.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.soptkathon.data.remote.response.ResponseMain
+import com.sopt.soptkathon.databinding.ItemFriendListBinding
+
+class MainAdapter : ListAdapter<ResponseMain.Data, MainAdapter.MainViewHolder>(MainComparator()) {
+
+    class MainViewHolder(private val binding: ItemFriendListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ResponseMain.Data) {
+            binding.friendData = data
+        }
+    }
+
+    private class MainComparator() : DiffUtil.ItemCallback<ResponseMain.Data>() {
+        override fun areItemsTheSame(
+            oldItem: ResponseMain.Data,
+            newItem: ResponseMain.Data
+        ): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ResponseMain.Data,
+            newItem: ResponseMain.Data
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemFriendListBinding.inflate(layoutInflater, parent, false)
+        return MainViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        return holder.onBind(getItem(position))
+    }
+}
