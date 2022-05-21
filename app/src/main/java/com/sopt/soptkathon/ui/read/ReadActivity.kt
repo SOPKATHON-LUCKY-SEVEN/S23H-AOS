627210c4940ea938fbbb0f18fe61ebe713d9e788
KWY0218 <wodrk1001@gmail.com>
2022-05-22 06:27:52 +0900

    [FEAT] Read Ui final

--- app/src/main/java/com/sopt/soptkathon/ui/read/ReadActivity.kt
+++ app/src/main/java/com/sopt/soptkathon/ui/read/ReadActivity.kt
@@ -1,12 +1,14 @@
 package com.sopt.soptkathon.ui.read
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.sopt.soptkathon.MainApp
 import com.sopt.soptkathon.R
 import com.sopt.soptkathon.databinding.ActivityReadBinding
+import com.sopt.soptkathon.util.CustomDialog
 
 class ReadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReadBinding
@@ -21,7 +23,31 @@ class ReadActivity : AppCompatActivity() {
                 vm = viewModel
                 lifecycleOwner = this@ReadActivity
             }
+        viewModel.getList()
+        with(binding){
+            val count = viewModel.letterList.value.size
+            ivReadStar1.visibility = if(count>0) View.INVISIBLE else View.VISIBLE
+            ivReadStar2.visibility = if(count>1) View.INVISIBLE else View.VISIBLE
+            ivReadStar3.visibility = if(count>2) View.INVISIBLE else View.VISIBLE
+            ivReadStar4.visibility = if(count>3) View.INVISIBLE else View.VISIBLE
 
-        
+            ivReadStar1.setOnClickListener {
+                val customDialog = CustomDialog(this@ReadActivity)
+                customDialog.showDialog(R.layout.dialog_letter, viewModel.letterList.value[0].content, viewModel.letterList.value[0].sender)
+            }
+            ivReadStar2.setOnClickListener {
+                val customDialog = CustomDialog(this@ReadActivity)
+                customDialog.showDialog(R.layout.dialog_letter, viewModel.letterList.value[1].content, viewModel.letterList.value[1].sender)
+            }
+            ivReadStar3.setOnClickListener {
+                val customDialog = CustomDialog(this@ReadActivity)
+                customDialog.showDialog(R.layout.dialog_letter, viewModel.letterList.value[2].content, viewModel.letterList.value[2].sender)
+            }
+            ivReadStar4.setOnClickListener {
+                val customDialog = CustomDialog(this@ReadActivity)
+                customDialog.showDialog(R.layout.dialog_letter, viewModel.letterList.value[3].content, viewModel.letterList.value[3].sender)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/sopt/soptkathon/ui/read/ReadViewModel.kt
+++ app/src/main/java/com/sopt/soptkathon/ui/read/ReadViewModel.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class ReadViewModel constructor(
-    userRepository: LoginRepository
+    private val userRepository: LoginRepository
 ) : ViewModel() {
     private val _user: MutableStateFlow<Pair<String?, String?>> = MutableStateFlow(Pair("", ""))
     private val _letterList: MutableStateFlow<List<ResponseLetter>> = MutableStateFlow(emptyList())
@@ -25,17 +25,22 @@ class ReadViewModel constructor(
             userRepository.getUser().collect {
                 _user.value = it!!
             }
-            val response = userRepository.getLetterList("")
+        }
+    }
+
+    fun getList() {
+        viewModelScope.launch {
+            val response = userRepository.getLetterList(user.value.first!!)
             if (response.isSuccessful) {
                 _letterList.value = response.body()!!.data!!
-                _titleText.value = String.format(
-                    "이번 주 %s 님의 밤하늘은 \n%d명의 감사로 빛나고 있어요",
-                    user.value.first,
-                    letterList.value.size
-                )
             } else {
                 Log.d("ViewModelssss", "viewmodel : ${response.errorBody()!!.string()}")
             }
+            _titleText.value = String.format(
+                "이번 주 %s 님의 밤하늘은 \n%d명의 감사로 빛나고 있어요",
+                user.value.second,
+                letterList.value.size
+            )
         }
     }
 }
