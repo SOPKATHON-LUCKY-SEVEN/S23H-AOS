7c60fc859e6d4a29608e7d27ee3af872496a104e
s9hn <tnghqn@naver.com>
2022-05-21 23:59:34 +0900

    [Feat] WriteActivity 생성

---
+++ app/src/main/java/com/sopt/soptkathon/ui/write/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.soptkathon.ui.write
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.soptkathon.R
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
