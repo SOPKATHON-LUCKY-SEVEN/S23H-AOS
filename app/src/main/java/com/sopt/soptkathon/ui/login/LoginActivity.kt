40b2ec8cdea92aa345a0e7404e1c9f411caec453
KWY0218 <wodrk1001@gmail.com>
2022-05-21 22:22:11 +0900

    [FEAT] Login 1st commit

---
+++ app/src/main/java/com/sopt/soptkathon/ui/login/LoginActivity.kt
@@ -0,0 +1,52 @@
+package com.sopt.soptkathon.ui.login
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.sopt.soptkathon.R
+import com.sopt.soptkathon.databinding.ActivityLoginBinding
+import kotlinx.coroutines.launch
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =
+            DataBindingUtil.setContentView<ActivityLoginBinding?>(this, R.layout.activity_main)
+                .apply {
+                    vm = viewModel
+                    lifecycleOwner = this@LoginActivity
+                }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.eventFlow.collect { event ->
+                    handleEvent(event)
+                }
+            }
+        }
+    }
+
+    private fun handleEvent(loginEvent: LoginEvent) {
+        when (loginEvent) {
+            is LoginEvent.ShowToast -> {
+                Log.d("asdfasdfs", "ShowToast")
+                Toast.makeText(
+                    this,
+                    loginEvent.msg,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            is LoginEvent.GoMain -> {
+                // 이동
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/sopt/soptkathon/ui/login/LoginViewModel.kt
@@ -0,0 +1,51 @@
+package com.sopt.soptkathon.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import java.util.regex.Pattern
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class LoginViewModel : ViewModel() {
+    private val _eventFlow = MutableSharedFlow<LoginEvent>()
+    private val _name = MutableStateFlow("")
+    private val _phoneNumber = MutableStateFlow("")
+
+    val name get() = _name
+    val phoneNumber get() = _phoneNumber
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun login() {
+        if (validate(name.value, phoneNumber.value) != null) {
+            emitEvent(LoginEvent.ShowToast(validate(name.value, phoneNumber.value)!!))
+        } else {
+            emitEvent(LoginEvent.GoMain)
+        }
+    }
+
+    private fun validate(name: String, phoneNumber: String): String? {
+        return when {
+            name.isEmpty() -> "이름을 입력해주세요"
+            !Pattern.matches("^[가-힣]*\$", name) -> "유효한 이름을 입력해주세요"
+            phoneNumber.isEmpty() -> "비밀번호를 입력해주세요"
+            !Pattern.matches(
+                "^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})\$",
+                phoneNumber
+            ) -> "유효한 이름을 입력해주세요"
+            else -> null
+        }
+    }
+
+    private fun emitEvent(event: LoginEvent) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+}
+
+sealed class LoginEvent {
+    data class ShowToast(val msg: String) : LoginEvent()
+    object GoMain : LoginEvent()
+}
